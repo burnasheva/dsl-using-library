77c7b3596d76650e669f4482197524c4c900a750
admin <admin@TeamCity>
2019-12-20 12:14:55 +0100

    TeamCity change in 'versioned settings / Use Library from a private Maven Registry' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,60 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetBuild
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetRestore
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetTest
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2019.2"
+
+project {
+
+    subProject(NetCli)
+}
+
+
+object NetCli : Project({
+    name = ".NET CLI"
+
+    buildType(NetCli_RunTests)
+})
+
+object NetCli_RunTests : BuildType({
+    name = "run tests"
+
+    vcs {
+        root(AbsoluteId("DotnetCore30"))
+    }
+
+    steps {
+        dotnetRestore {
+            projects = "MSTestCore.sln"
+        }
+        dotnetBuild {
+            projects = "MSTestCore.sln"
+        }
+        dotnetTest {
+            projects = "PrimeService.Tests/PrimeService.Tests.csproj"
+        }
+    }
+})
